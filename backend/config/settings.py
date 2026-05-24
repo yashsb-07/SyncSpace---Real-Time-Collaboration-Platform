fb4aab83f2b91a922ce958bc4595805160a8f3ec
yashsb-07 <yashsbansode07@gmail.com>
2026-05-24 23:09:59 +0530

    Configure MySQL database settings for Django project

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'syncspace',
+        'USER': 'root',
+        'PASSWORD': 'Y@$#$b07',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
