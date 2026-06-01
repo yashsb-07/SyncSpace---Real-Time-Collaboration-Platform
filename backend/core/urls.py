af38055470a2f9d94ee9ec7c653736dd80842e3e
yashsb-07 <yashsbansode07@gmail.com>
2026-06-01 20:11:46 +0530

    feat: create backend status endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, status_api
 
 urlpatterns = [
     path('hello/', hello_world),
+    path('status/', status_api),
 ]
