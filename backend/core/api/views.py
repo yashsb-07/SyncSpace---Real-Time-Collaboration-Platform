40209daf3553edd4e2e58806cdee3a53b4f9cc07
Yash Bansode <yashsbansode07@gmail.com>
2026-08-15 23:00:42 +0530

    Comment out hello_world and status_api functions
    Comment out API view functions in views.py.

--- backend/core/api/views.py
+++ backend/core/api/views.py
@@ -1,16 +1,16 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 
 
-@api_view(['GET'])
-def hello_world(request):
-    return Response({
-        "message": "Hello from Django Backend!"
-    })
+# @api_view(['GET'])
+# def hello_world(request):
+#     return Response({
+#         "message": "Hello from Django Backend!"
+#     })
 
 
-@api_view(['GET'])
-def status_api(request):
-    return Response({
-        "status": "Backend working perfectly"
-    })
+# @api_view(['GET'])
+# def status_api(request):
+#     return Response({
+#         "status": "Backend working perfectly"
+#     })
